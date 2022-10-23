d1bfb0c1e880834d62a001ccabe23ee03c3836f1
AnirudhaBS <anirudhabs42@gmail.com>
2022-10-23 21:00:47 +0530

    DOM

--- index.js
+++ index.js
@@ -96,74 +96,142 @@
 
 //09-10-2022
 //let & Const---These two are the block scope.
-let x = 10; //let cannot be Redeclared.
+// let x = 10; //let cannot be Redeclared.
 
-const PI = 3.141592653589793;//Const cannot be Redeclared, Reassigned.
-console.log(PI);
-//Const in Array
-const cars = ["Saab", "Volvo", "BMW"];
-console.log(cars);
-cars[0] = "Toyota";// You can change an element
-cars.push("Audi");// You can add an element
-console.log(cars);
+// const PI = 3.141592653589793;//Const cannot be Redeclared, Reassigned.
+// console.log(PI);
+// //Const in Array
+// const cars = ["Saab", "Volvo", "BMW"];
+// console.log(cars);
+// cars[0] = "Toyota";// You can change an element
+// cars.push("Audi");// You can add an element
+// console.log(cars);
 
-//Array Functions
-function add(a,b) {
-    return a+b
-}// Function Expression
-console.log(add(10,80));
+// //Array Functions
+// function add(a,b) {
+//     return a+b
+// }// Function Expression
+// console.log(add(10,80));
 
-const sum =(a,b) => {return a + b}; //Arrow function.
-console.log(sum(500,6001));
+// const sum =(a,b) => {return a + b}; //Arrow function.
+// console.log(sum(500,6001));
 
-//Array Helpers
-//ForEach
-var arr=['one','two','three','four','five']
-arr.forEach(function (pri) {
-    console.log(pri);
-})
+// //Array Helpers
+// //ForEach
+// var arr=['one','two','three','four','five']
+// arr.forEach(function (pri) {
+//     console.log(pri);
+// })
 
-const arr1 = [3, 5, 6, 1];
-let sum1 = 0;
-  arr1.forEach((item) => {sum1 += item;// adding the numbers in an array using arrow function.
-  });
-console.log(sum1)
+// const arr1 = [3, 5, 6, 1];
+// let sum1 = 0;
+//   arr1.forEach((item) => {sum1 += item;// adding the numbers in an array using arrow function.
+//   });
+// console.log(sum1)
 
-//map
-const num=[1,2,3,4,5,6,7,8,9]
-num.map((num)=> console.log(num*2))
+// //map
+// const num=[1,2,3,4,5,6,7,8,9]
+// num.map((num)=> console.log(num*2))
 
-//Filter
-const arr2 = [3, 5, 6, 1];
-const req = arr2.filter((item) => {
-    return item > 4
-  });
-console.log(req);
-console.log(arr2);
+// //Filter
+// const arr2 = [3, 5, 6, 1];
+// const req = arr2.filter((item) => {
+//     return item > 4
+//   });
+// console.log(req);
+// console.log(arr2);
 
-//reduce
-const arrr = [3, 5, 6, 1];
-const req2 = arrr.reduce((acc, curr) => {
-    acc *= curr;
-    return acc;
-  });
-console.log(req2);
+// //reduce
+// const arrr = [3, 5, 6, 1];
+// const req2 = arrr.reduce((acc, curr) => {
+//     acc *= curr;
+//     return acc;
+//   });
+// console.log(req2);
 
-//find
-const array = [10, 20, 30, 40];
-const found = array.find((elem) => {
-  return elem > 20
-});
-console.log(found);
+// //find
+// const array = [10, 20, 30, 40];
+// const found = array.find((elem) => {
+//   return elem > 20
+// });
+// console.log(found);
 
-//String literals
-var first='happy';
-var second='weekend';
-const message=`${first}  ${second}`
-console.log(message);
-
-
+// //String literals
+// var first='happy';
+// var second='weekend';
+// const message=`${first}  ${second}`
+// console.log(message);
 
 
 
 
+
+  
+// program to create a simple calculator using the if...else...if in JavaScript.  
+// take the operator from the user through prompt box in JavaScript.  
+// const operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): ');  
+  
+// // accept the number from the user through prompt box  
+// const number1 = parseFloat(prompt ('Enter the first number: '));  
+// const number2 = parseFloat(prompt ('Enter the second number: '));  
+  
+// let result; // declaration of the variable.  
+  
+// // use if, elseif and else keyword to define the calculator condition in JavaScript.  
+// if (operator == '+') { // use + (addition) operator to add two numbers  
+//     result = number1 + number2;  
+// }  
+// else if (operator == '-') { // use -  (subtraction) operator to subtract two numbers  
+//     result = number1 - number2;  
+// }  
+// else if (operator == '*') { // use * (multiplication) operator to multiply two numbers  
+//     result = number1 * number2;  
+// }  
+// else {  
+//     result = number1 / number2; // use / (division) operator to divide two numbers  
+// }  
+  
+// // display the result of the Calculator  
+// window.alert(" Result is" + result);  
+
+
+
+
+//API 
+
+//DOM
+
+// const textarr= document.getElementById("head")
+// console.log(textarr);
+
+// function dyn(){
+//     const name = document.querySelector("H1")
+//     name.innerText="Welcome to JS DOM";
+// }
+
+function increment() {
+    const initialValue= document.querySelector("h1");
+    const updatedValue= Number(initialValue.innerText)+ 1;
+    initialValue.innerText=updatedValue;
+    
+    if(updatedValue%5==0){
+        initialValue.style.color="red";
+    }
+    else{
+        initialValue.style.color="";
+    }
+}
+
+function decrement() {
+    // console.log("dec clicked");
+    const initialValue= document.querySelector("h1");
+    const updatedValue= Number(initialValue.innerText) - 1;
+    initialValue.innerText=updatedValue;
+    if(updatedValue%5==0){
+        initialValue.style.color="red";
+    }
+    else{
+        initialValue.style.color="";
+    }
+
+}
